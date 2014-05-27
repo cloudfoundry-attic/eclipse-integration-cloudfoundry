aea9fc8722ede18214ec45ec650df16261f14d68
eyuen <eyuen@ca.ibm.com>
2014-05-27 16:55:20 -0400

    [72048132] Define public APIs for adopters - add missing classes

---
+++ org.cloudfoundry.ide.eclipse.server.ui/src/org/cloudfoundry/ide/eclipse/server/ui/CloudUIUtil.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2014 IBM Corporation and others
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, 
+ * Version 2.0 (the "License”); you may not use this file except in compliance 
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *  
+ *  Contributors:
+ *     IBM Corporation - initial API and implementation
+ ********************************************************************************/
+package org.cloudfoundry.ide.eclipse.server.ui;
+
+import org.cloudfoundry.ide.eclipse.internal.server.ui.CloudUiUtil;
+
+public class CloudUIUtil {
+
+	/**
+	 * Open the given URL on a browser
+	 * @param url the URL value to be launched.
+	 */
+	public static void openUrl(String url) {
+		CloudUiUtil.openUrl(url);
+	}
+}
