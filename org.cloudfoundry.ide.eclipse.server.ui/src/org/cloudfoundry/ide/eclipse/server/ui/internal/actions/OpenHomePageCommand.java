1a315b807e34b8ffa98b281febee9d0111eea2a2
bcyeah <bcyeah@gmail.com>
2014-08-21 15:46:16 -0400

    [77316514] Allow user to choose application during Open Home Page

--- org.cloudfoundry.ide.eclipse.server.ui/src/org/cloudfoundry/ide/eclipse/server/ui/internal/actions/OpenHomePageCommand.java
+++ org.cloudfoundry.ide.eclipse.server.ui/src/org/cloudfoundry/ide/eclipse/server/ui/internal/actions/OpenHomePageCommand.java
@@ -20,22 +20,32 @@
 package org.cloudfoundry.ide.eclipse.server.ui.internal.actions;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.cloudfoundry.ide.eclipse.server.core.internal.CloudFoundryPlugin;
 import org.cloudfoundry.ide.eclipse.server.core.internal.CloudFoundryServer;
 import org.cloudfoundry.ide.eclipse.server.core.internal.client.CloudFoundryApplicationModule;
 import org.cloudfoundry.ide.eclipse.server.ui.internal.CloudUiUtil;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.window.Window;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.dialogs.ElementListSelectionDialog;
+import org.eclipse.wst.server.core.IModule;
 import org.eclipse.wst.server.core.IServer;
 import org.eclipse.wst.server.core.model.IURLProvider;
 
 public class OpenHomePageCommand extends BaseCommandHandler {
 
-	private URL homePageUrl;
+	private String SELECTION_TEXT = "Select the module to launch home page from";
 	
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		initializeSelection(event);
-		
+
 		if (selectedServer != null) {
 			CloudFoundryApplicationModule cloudModule = getSelectedCloudAppModule();
 			if (cloudModule != null) {
@@ -43,15 +53,75 @@ public class OpenHomePageCommand extends BaseCommandHandler {
 				// Based on property testers, this should already be started
 				if (state == IServer.STATE_STARTED) {
 					IURLProvider cloudServer = (IURLProvider)selectedServer.loadAdapter(IURLProvider.class, null);
-					homePageUrl = cloudServer.getModuleRootURL(selectedModule);
-					if (homePageUrl != null) {
-						CloudUiUtil.openUrl(homePageUrl.toExternalForm());
+
+					CloudFoundryServer cfs = (CloudFoundryServer)selectedServer.loadAdapter(CloudFoundryServer.class, null);
+					
+					String contextRoot = null;
+					if (cfs != null){
+						// IModule[][] because IModule[] is the correct representation of module structure
+						IModule[][] launchables = cfs.getLaunchableModules(selectedModule);
+
+						if (launchables != null){
+							if (launchables.length == 1){
+								contextRoot = cfs.getLaunchableModuleContextRoot(launchables[0]);
+							}
+							else if (launchables.length > 1 ){
+								
+								List<String> selectionOptions = new ArrayList<String>();
+								Map<String, String> index = new HashMap<String, String>();
+								for (int i = 0; i < launchables.length; i++){
+									String option = "";
+									for (int j = 0; j < launchables[i].length; j++){
+										option += launchables[i][j].getName() + "/";
+									}
+									if (option.endsWith("/")){
+										option = option.substring(0, option.length() - 1);
+									}
+									selectionOptions.add(option);
+									index.put(option, String.valueOf(i));
+								}
+								
+								ElementListSelectionDialog dialog = new ElementListSelectionDialog(PlatformUI.getWorkbench().getDisplay().getActiveShell(), new LabelProvider());
+								dialog.setTitle(SELECTION_TEXT);
+								dialog.setElements(selectionOptions.toArray());
+
+
+								if (dialog.open() != Window.OK) { 	
+									if (dialog.getReturnCode() == Window.CANCEL){
+										CloudFoundryPlugin.logWarning("User pressed cancel on selection dialog");
+										return null;
+									}
+									CloudFoundryPlugin.logError(("Failed to open the Open Home Page selection dialog"));
+									return null;
+								}
+
+								Object[] result = dialog.getResult();
+								contextRoot = cfs.getLaunchableModuleContextRoot(launchables[Integer.valueOf(index.get(result[0]))]); 
+							}
+						}
 					}
+					
+					try {
+						URL homePageUrl = cloudServer.getModuleRootURL(selectedModule);
+						if (contextRoot != null){
+							homePageUrl = new URL(homePageUrl, contextRoot);
+						}
+						
+						if (homePageUrl != null) {
+							CloudUiUtil.openUrl(homePageUrl.toExternalForm());
+						}
+						else {
+							CloudFoundryPlugin.logError("homePageUrl is null, unable to launch the Home Page URL");
+							return null;
+						}
+					}
+					catch (Exception e) {
+						CloudFoundryPlugin.logError("Cannot launch the home page URL", e);
+						return null;
+					}					
 				}
 			}
 		}
-
-		
 		return null;
 	}
 	
