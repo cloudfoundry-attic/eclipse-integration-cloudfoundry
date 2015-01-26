e0bbafa8ff2d5cc911082ed68d886cb5cc230811
eyuen <eyuen@ca.ibm.com>
2015-01-26 16:55:24 -0500

    [78669150] NPE when opening CF context menu actions in Servers view

--- org.cloudfoundry.ide.eclipse.server.ui/src/org/cloudfoundry/ide/eclipse/server/ui/internal/actions/BaseCommandHandler.java
+++ org.cloudfoundry.ide.eclipse.server.ui/src/org/cloudfoundry/ide/eclipse/server/ui/internal/actions/BaseCommandHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 Pivotal Software, Inc. 
+ * Copyright (c) 2014,2015 Pivotal Software, Inc. 
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Apache License, 
@@ -19,13 +19,16 @@
  ********************************************************************************/
 package org.cloudfoundry.ide.eclipse.server.ui.internal.actions;
 
+import org.cloudfoundry.ide.eclipse.server.ui.internal.Logger;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchPartSite;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.wst.server.core.IModule;
 import org.eclipse.wst.server.core.IServer;
@@ -40,21 +43,40 @@ public abstract class BaseCommandHandler extends AbstractHandler implements IHan
 	// Must first init selected server or module.  Or just override execute
 	protected void initializeSelection(ExecutionEvent event) throws ExecutionException {
 		IWorkbenchPart activePart = HandlerUtil.getActivePart(event);
-		ISelection selection = activePart.getSite().getSelectionProvider().getSelection();
-		if (selection instanceof IStructuredSelection) {
-			Object obj = ((IStructuredSelection) selection).getFirstElement();
-			if (obj instanceof IServer) {
-				this.selectedServer = (IServer) obj;
-			}
-			else if (obj instanceof IServerModule) {
-				IServerModule sm = (IServerModule) obj;
-				IModule[] module = sm.getModule();
-				this.selectedModule = module[module.length - 1];
-				if (this.selectedModule != null) {
-					this.selectedServer = sm.getServer();
+		if (activePart != null) {
+			IWorkbenchPartSite partSite = activePart.getSite();
+			if (partSite != null) {
+				ISelectionProvider selectionProvider = partSite.getSelectionProvider();
+				if (selectionProvider != null) {
+					ISelection selection = selectionProvider.getSelection();
+					if (selection instanceof IStructuredSelection) {
+						Object obj = ((IStructuredSelection) selection).getFirstElement();
+						if (obj instanceof IServer) {
+							this.selectedServer = (IServer) obj;
+						}
+						else if (obj instanceof IServerModule) {
+							IServerModule sm = (IServerModule) obj;
+							IModule[] module = sm.getModule();
+							this.selectedModule = module[module.length - 1];
+							if (this.selectedModule != null) {
+								this.selectedServer = sm.getServer();
+							}
+						}
+					}
+				} else {
+					logSelectionDetectionFailure();
 				}
+			} else {
+				logSelectionDetectionFailure();
 			}
+		} else {
+			logSelectionDetectionFailure();
 		}
 	}
 
+	private void logSelectionDetectionFailure() {
+    	if (Logger.WARNING) {
+    		Logger.println(Logger.WARNING_LEVEL, this, "logSelectionDetectionFailure", "Failed to initialize command selection."); //$NON-NLS-1$ //$NON-NLS-2$
+    	}
+	}
 }
