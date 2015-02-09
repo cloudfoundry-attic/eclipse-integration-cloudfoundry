89d3bde1cf6ad62ef23dbd75a1a4dfaec04cf1d7
Nieraj Singh <nsingh@gopivotal.com>
2015-02-09 00:44:27 -0800

    [87914508] - Fix to handle OAuth2AccessDeniedException 403 error when performing a client request.

--- org.cloudfoundry.ide.eclipse.server.core/src/org/cloudfoundry/ide/eclipse/server/core/internal/CloudErrorUtil.java
+++ org.cloudfoundry.ide.eclipse.server.core/src/org/cloudfoundry/ide/eclipse/server/core/internal/CloudErrorUtil.java
@@ -31,6 +31,7 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.osgi.util.NLS;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.oauth2.client.resource.OAuth2AccessDeniedException;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClientException;
@@ -128,15 +129,28 @@ public class CloudErrorUtil {
 	/**
 	 * Error due to invalid credentials, typically 401 or 403 HTTP errors.
 	 * Returns null if the error is NOT an invalid credentials error.
-	 * @param t error to parse
+	 * @param error error to parse
 	 * @return Error message if invalid credentials error (401 or 403), or null.
 	 */
-	public static String getInvalidCredentialsError(Throwable t) {
-		if (isUnauthorisedException(t)) {
+	public static String getInvalidCredentialsError(Throwable error) {
+		if (isUnauthorisedException(error)) {
 			return Messages.ERROR_WRONG_EMAIL_OR_PASSWORD_UNAUTHORISED;
 		}
-		else if (isForbiddenException(t)) {
+		else if (isForbiddenException(error)) {
 			return Messages.ERROR_WRONG_EMAIL_OR_PASSWORD_FORBIDDEN;
+		}
+		else {
+			OAuth2AccessDeniedException oauthException = null;
+
+			if (error instanceof OAuth2AccessDeniedException) {
+				oauthException = (OAuth2AccessDeniedException) error;
+			}
+			else if (error.getCause() instanceof OAuth2AccessDeniedException) {
+				oauthException = (OAuth2AccessDeniedException) error.getCause();
+			}
+			if (oauthException != null && oauthException.getHttpErrorCode() == 403) {
+				return NLS.bind(Messages.ERROR_ACCESS_TOKEN, oauthException.getOAuth2ErrorCode());
+			}
 		}
 		return null;
 	}
