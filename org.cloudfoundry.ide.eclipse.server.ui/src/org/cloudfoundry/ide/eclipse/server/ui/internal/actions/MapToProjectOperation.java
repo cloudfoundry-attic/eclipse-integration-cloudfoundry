b019de49a520111b7b78c2c7c411600f6df903b5
Nieraj Singh <nsingh@gopivotal.com>
2015-03-19 11:13:02 -0700

    [78666960] - Project selection should allow non-Java projects to be selected (e.g. Node)

--- org.cloudfoundry.ide.eclipse.server.ui/src/org/cloudfoundry/ide/eclipse/server/ui/internal/actions/MapToProjectOperation.java
+++ org.cloudfoundry.ide.eclipse.server.ui/src/org/cloudfoundry/ide/eclipse/server/ui/internal/actions/MapToProjectOperation.java
@@ -21,7 +21,6 @@ package org.cloudfoundry.ide.eclipse.server.ui.internal.actions;
 
 import org.cloudfoundry.ide.eclipse.server.core.internal.CloudErrorUtil;
 import org.cloudfoundry.ide.eclipse.server.core.internal.CloudFoundryPlugin;
-import org.cloudfoundry.ide.eclipse.server.core.internal.CloudFoundryProjectUtil;
 import org.cloudfoundry.ide.eclipse.server.core.internal.CloudFoundryServer;
 import org.cloudfoundry.ide.eclipse.server.core.internal.ModuleCache;
 import org.cloudfoundry.ide.eclipse.server.core.internal.client.CloudFoundryApplicationModule;
@@ -31,15 +30,9 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.internal.ui.wizards.TypedElementSelectionValidator;
-import org.eclipse.jdt.internal.ui.wizards.TypedViewerFilter;
-import org.eclipse.jdt.ui.JavaElementComparator;
-import org.eclipse.jdt.ui.JavaElementLabelProvider;
-import org.eclipse.jdt.ui.StandardJavaElementContentProvider;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerFilter;
 import org.eclipse.jface.window.Window;
@@ -47,6 +40,8 @@ import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.dialogs.ElementTreeSelectionDialog;
+import org.eclipse.ui.model.WorkbenchLabelProvider;
+import org.eclipse.ui.model.WorkbenchViewerComparator;
 import org.eclipse.wst.server.core.IModule;
 import org.eclipse.wst.server.core.IServer;
 import org.eclipse.wst.server.core.IServerWorkingCopy;
@@ -139,61 +134,83 @@ public class MapToProjectOperation implements ICloudFoundryOperation {
 	 */
 	protected IProject selectReplaceProject(final CloudFoundryServer cloudServer,
 			final CloudFoundryApplicationModule appModule) {
-		Class<?>[] acceptedClasses = new Class[] { IJavaProject.class, IProject.class };
-		TypedElementSelectionValidator selectionValidator = new TypedElementSelectionValidator(acceptedClasses, false);
 
-		acceptedClasses = new Class[] { IJavaProject.class, IProject.class };
-		ViewerFilter viewerFilter = new TypedViewerFilter(acceptedClasses) {
+		ViewerFilter viewerFilter = new ViewerFilter() {
 			/**
 			 * {@inheritDoc}
 			 */
 			@Override
 			public boolean select(Viewer viewer, Object parent, Object element) {
 				IProject potentialProject = null;
-				if (element instanceof IJavaProject) {
-					potentialProject = ((IJavaProject) element).getProject();
-				}
-				else if (element instanceof IProject && CloudFoundryProjectUtil.isJavaProject((IProject) element)) {
+				if (element instanceof IProject) {
 					potentialProject = (IProject) element;
 				}
-
-				if (null != potentialProject) {
-
-					IModule[] allModules = cloudServer.getServerOriginal().getModules();
-					if (allModules != null) {
-						for (IModule mod : allModules) {
-							if (mod.getProject() != null && mod.getProject().equals(potentialProject)) {
-								return false;
-							}
+				if (potentialProject == null || !potentialProject.isAccessible()) {
+					return false;
+				}
+				IModule[] allModules = cloudServer.getServerOriginal().getModules();
+				if (allModules != null) {
+					for (IModule mod : allModules) {
+						if (mod.getProject() != null && mod.getProject().equals(potentialProject)) {
+							return false;
 						}
 					}
-					return true;
 				}
-				return false;
+				return true;
 			}
 
 		};
 
-		StandardJavaElementContentProvider contentProvider = new StandardJavaElementContentProvider();
-		ILabelProvider labelProvider = new JavaElementLabelProvider(JavaElementLabelProvider.SHOW_DEFAULT);
+		ITreeContentProvider contentProvider = new ITreeContentProvider() {
+
+			@Override
+			public void inputChanged(Viewer viewer, Object ob1, Object ob2) {
+			}
+
+			@Override
+			public void dispose() {
+			}
+
+			@Override
+			public boolean hasChildren(Object ob1) {
+				return false;
+			}
+
+			@Override
+			public Object getParent(Object ob1) {
+				return null;
+			}
+
+			@Override
+			public Object[] getElements(Object elements) {
+				if (elements instanceof IProject[]) {
+					return (IProject[]) elements;
+				}
+				return null;
+			}
+
+			@Override
+			public Object[] getChildren(Object parent) {
+				return null;
+			}
+		};
+
+		ILabelProvider labelProvider = new WorkbenchLabelProvider();
 		ElementTreeSelectionDialog dialog = new ElementTreeSelectionDialog(shell, labelProvider, contentProvider);
-		dialog.setValidator(selectionValidator);
-		dialog.setComparator(new JavaElementComparator());
+		dialog.setComparator(new WorkbenchViewerComparator());
+
 		dialog.setTitle(Messages.MapToProjectOperation_PROJECT_SELECTION_DIALOGUE_TITLE);
 		dialog.setMessage(NLS.bind(Messages.MapToProjectOperation_PROJECT_SELECTION_DIALOGUE_MESSAGE,
 				appModule.getDeployedApplicationName()));
 		dialog.addFilter(viewerFilter);
-		dialog.setInput(JavaCore.create(ResourcesPlugin.getWorkspace().getRoot()));
+		dialog.setInput(ResourcesPlugin.getWorkspace().getRoot().getProjects());
 		dialog.setInitialSelection(null);
 		dialog.setHelpAvailable(false);
 
 		IProject selectProj = null;
 		if (dialog.open() == Window.OK) {
 			Object selectObj = dialog.getFirstResult();
-			if (selectObj instanceof IJavaProject) {
-				selectProj = ((IJavaProject) selectObj).getProject();
-			}
-			else if (selectObj instanceof IProject) {
+			if (selectObj instanceof IProject) {
 				selectProj = (IProject) selectObj;
 			}
 			final boolean[] result = new boolean[1];
